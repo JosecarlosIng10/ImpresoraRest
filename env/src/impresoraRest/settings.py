6bd12c5c6944f9f76cee480d7041cb62f1d8798f
JosecarlosIng10 <201212644@ingenieria.usac.edu.gt>
2019-09-02 20:46:45 +0000

    Configuracion para usar api en pythonanywhere

--- env/src/impresoraRest/settings.py
+++ env/src/impresoraRest/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'impresoraRest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'printcontroller.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR, 'printcontroller.db'),                # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
